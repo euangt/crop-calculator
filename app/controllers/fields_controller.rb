3dc73a229f7418fc0125a9479e9f2baf3c56a6bd
euangt <euan.gillespie@outlook.com>
2021-03-06 14:59:10 +0000

    fields view added

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,2 +1,25 @@
 class FieldsController < ApplicationController
+  before_action :find_farm, only: [ :edit, :update]
+
+  def index
+    @fields = Field.all
+  
+  end
+    
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_farm
+    @farm = Farm.find(params[:id])
+  end
+
+  #prevent passing of malicious code into db
+  def farm_params
+    params.require(:field).require(:name, :crop, :last_sprayed)
+  end
 end
