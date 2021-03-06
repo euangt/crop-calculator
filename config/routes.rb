b35373558b4eac228eccaade381c951abcf9e6a5
euangt <euan.gillespie@outlook.com>
2021-03-06 14:56:24 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => "fields#index"
+  resources :farms do
+    resources :fields, only: [:index, :edit, :update]
+  end
 end
