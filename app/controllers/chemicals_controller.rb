f9b5f185c4df1b09768ac9961f7bc2b2ea28ada7
euangt <euan.gillespie@outlook.com>
2021-03-07 19:52:36 +0000

    view complete

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -3,5 +3,11 @@ class ChemicalsController < ApplicationController
     @fields = Field.all
     @hectarage = 0
     @crops = Crop.all
+    @date = Date.today
+    # @offset = @chemical.application_frequency * 7
   end
+  #ToDo
+  # Find earliest last_sprayed date from each field
+  # Add chemical.application_frequency to that for next_spray 
+  # and minus 7 for next_order
 end
