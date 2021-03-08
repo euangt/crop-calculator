7f7b15c8052e63d2f0c192ff2fdd7cb12205624a
euangt <euan.gillespie@outlook.com>
2021-03-08 14:26:32 +0000

    crop show view added, logic moved functions

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -3,7 +3,32 @@ class CropsController < ApplicationController
     @fields = Field.all
     @hectarage = 0
     @crops = Crop.all
-
   end
 
+  def show 
+    @crop = Crop.find(params[:id])
+    @fields = Field.where(crop_id: @crop.id)
+    @hectarage = hectarage_count(@fields)
+    @chemical = Chemical.find(@crop.chemical_id)
+    @date = earliest_spray(@fields)
+    
+  end
+
+  private 
+  
+  def earliest_spray(fields)
+    date = Date.today
+    fields.each do |field|
+      field.last_sprayed < date ? date = field.last_sprayed : date
+    end
+    return date
+  end
+
+  def hectarage_count(fields)
+    hectarage = 0
+    fields.each do |field|
+      hectarage += field.hectarage
+    end
+    return hectarage
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'chemicals/index'
   root :to => "crops#index"
   resources :fields, only: [:index, :show ]
-  resources :crops, only: [ :index ]
+  resources :crops, only: [ :index, :show ]
 end
