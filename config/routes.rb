3bf142cc266d807e101d9558b2352c91d3102f97
euangt <euan.gillespie@outlook.com>
2021-03-08 13:07:37 +0000

    code tidied

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'chemicals/index'
   root :to => "crops#index"
-    resources :fields, only: [:index, :show, :edit, :update]
+  resources :fields, only: [:index, :show ]
   resources :crops, only: [ :index ]
 end
