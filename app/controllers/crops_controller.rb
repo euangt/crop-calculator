f32a8143f9886df088153a2f6cb8703673c9752d
euangt <euan.gillespie@outlook.com>
2021-03-08 14:41:40 +0000

    final edits to crop/show

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -8,15 +8,17 @@ class CropsController < ApplicationController
   def show 
     @crop = Crop.find(params[:id])
     @fields = Field.where(crop_id: @crop.id)
+    
     @hectarage = hectarage_count(@fields)
     @chemical = Chemical.find(@crop.chemical_id)
     @date = earliest_spray(@fields)
-    
+    @order_date = @crop.next_order(@date, @chemical)
   end
 
   private 
-  
+  # For each crop, find the earliest spray.
   def earliest_spray(fields)
+    # Arbitrary date, in expanded programme this would be set to most recent order/delivery date
     date = Date.today
     fields.each do |field|
       field.last_sprayed < date ? date = field.last_sprayed : date
@@ -24,6 +26,7 @@ class CropsController < ApplicationController
     return date
   end
 
+  # For each crop find the hectarage it covers
   def hectarage_count(fields)
     hectarage = 0
     fields.each do |field|
